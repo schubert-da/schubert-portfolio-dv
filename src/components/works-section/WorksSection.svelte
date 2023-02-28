bb4b622528db5d2d6cad87e5f088d7c16414c967
schubert <schubertdeabreu2@gmail.com>
2023-02-28 10:13:13 +0530

    added project components

--- src/components/works-section/WorksSection.svelte
+++ src/components/works-section/WorksSection.svelte
@@ -1,44 +1,11 @@
 <script>
+    import Kaggle22 from "./kaggle-22/Kaggle22.svelte";
 </script>
 
 <div class="container">
     <div class="section-header" id="works-section">Projects</div>
 
-    <!-- <div class="works-section">
-        <div class="works-header-title">State of Low-Code in Data</div>
-        <div class="works-images">
-            <div class="works-image">
-                <img src="images/projects/kaggle22_main.png" alt="img21" />
-            </div>
-            <div class="works-image">
-                <img src="images/projects/kaggle22_table2.png" alt="img21" />
-            </div>
-        </div>
-        <div class="works-text">
-            <div class="works-text-header">
-                <div class="works-header-subtitle">
-                    For my Kaggle ML and DS Survey 2022 submission, I looked at
-                    how data professionals were using low-code tools to quickly
-                    build and deploy data pipelines without the need for complex
-                    code.
-                </div>
-            </div>
-            <div class="works-text-desc">
-                <div class="works-desc-content">
-                    This competition entry won a runner-up spot in the
-                    competition. It used data from various low-code trend
-                    industry reports as well as Kaggle survey data to look into
-                    the usage of these tools.
-                    <br /><br />
-                    The notebook contains graphs in Python, with more involved graphs
-                    and tables using <strong>Markdown</strong> +
-                    <strong>CSS</strong>.
-                    <br />
-                    <a href="https://google.com" class="btn work-link" target="_blank" rel="noreferrer">View notebook</a>
-                </div>
-            </div>
-        </div>
-    </div> -->
+    <Kaggle22 />
 </div>
 
 <style>
@@ -51,5 +18,112 @@
         padding-bottom: 1rem;
         margin-bottom: 1rem;
         text-transform: uppercase;
+    }
+
+    .container {
+        width: 80%;
+        margin: 4rem auto 0rem auto;
+    }
+
+    .container :global(.work-container) {
+        display: flex;
+        flex-direction: column;
+
+        border: 1px solid #999;
+        border-radius: 4px;
+        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
+            rgba(0, 0, 0, 0.23) 0px 6px 6px;
+
+        margin: 0rem 15% 6rem 5%;
+    }
+
+    .container :global(.work-container > div.details-container > div) {
+        padding: 2.5rem 2.5rem 2.5rem 2.5rem;
+    }
+
+    .container :global(.work-header-title) {
+        color: #444;
+        text-transform: uppercase;
+        letter-spacing: -1.2px;
+
+        font-size: 2.6rem;
+        font-weight: 700;
+        padding: 2rem !important;
+        border-bottom: 2px solid #999;
+    }
+
+    .container :global(.work-field-title) {
+        color: #444;
+        text-transform: uppercase;
+        letter-spacing: -1px;
+
+        font-size: 2.4rem;
+        font-weight: 600;
+        margin-top: 0;
+    }
+
+    .container :global(.work-field:not(:first-of-type) .work-field-title) {
+        margin-top: 2.6rem;
+    }
+
+    .container :global(.work-field-desc) {
+        color: #444;
+        max-width: 55ch;
+        margin-top: 5px;
+
+        font-size: 1.6rem;
+        line-height: 1.2;
+    }
+
+    /* EXPANSION PANEL STYLING */
+    .container :global(.details-container) {
+        opacity: 0;
+        height: auto;
+        max-height: 0;
+    }
+
+    .container :global(#show-desc) {
+        display: none;
+    }
+
+    .container :global(#show-desc:checked + .details-container) {
+        opacity: 1;
+        max-height: 1200px;
+        transition: all 1s 0s ease-in;
+    }
+
+    .container :global(#show-desc:not(:checked) + .details-container) {
+        opacity: 0;
+        height: auto;
+        max-height: 0;
+        transition: max-height 0.5s 0s ease-out, opacity 0.2s 0s ease-out;
+    }
+
+    
+    .container :global(.chevron) {
+        border-style: solid;
+        border-width: 0.25em 0.25em 0 0;
+        content: "";
+        height: 0.45em;
+        position: relative;
+        top: 10px;
+        right: 10px;
+        float: right;
+
+        transform: rotate(-45deg);
+        vertical-align: top;
+        width: 0.45em;
+
+        transition: all 0.3s 0s ease-in-out;
+    }
+
+    /* GRID STYLING */
+    .container :global(.grid-item) {
+        width: auto;
+        height: auto;
+        max-width: 100%;
+        max-height: 100%;
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px,
+            rgba(0, 0, 0, 0.23) 0px 3px 6px;
     }
 </style>
---
+++ src/components/works-section/kaggle-22/Kaggle22.svelte
@@ -0,0 +1,129 @@
+<script>
+// @ts-nocheck
+
+    let project_name = "kaggle22";
+
+    function toggleExpansion() {
+        let checkBox = document.getElementById("show-desc");
+        let chevron = document.querySelector(".chevron.kaggle22");
+
+        if (checkBox.checked == true) {
+            chevron.style.transform = "rotate(135deg)";
+        } else {
+            chevron.style.transform = "rotate(-45deg)"; 
+        }
+    }
+</script>
+
+<div class="work-container">
+    <label for="show-desc">
+        <div class="work-header-title">
+            State of Low-Code in Data
+            <div class="chevron kaggle22" />
+        </div>
+    </label>
+    <input
+        type="checkbox"
+        name="show-desc"
+        id="show-desc"
+        on:click={toggleExpansion}
+    />
+    <div class="details-container">
+        <div class="work-text">
+            <div class="work-field">
+                <div class="work-field-title">Brief</div>
+                <div class="work-field-desc">
+                    For my Kaggle ML and DS Survey 2022 submission, I looked at
+                    how data professionals were using low-code tools to quickly
+                    build and deploy data pipelines without the need for complex
+                    code.
+                </div>
+            </div>
+
+            <div class="work-field">
+                <div class="work-field-title">My approach</div>
+                <div class="work-field-desc">
+                    This competition entry won a runner-up spot in the
+                    competition. It used data from various low-code trend
+                    industry reports as well as Kaggle survey data to look into
+                    the usage of these tools.
+                    <br /><br />
+                    The notebook contains graphs in Python, with more involved graphs
+                    and tables using <strong>Markdown</strong> +
+                    <strong>CSS</strong>.
+                </div>
+            </div>
+        </div>
+
+        <br />
+
+        <div class="results-grid">
+            <div class="grid-item grid-item--result">
+                <div class="work-field-title">The Result</div>
+                <div class="work-field-desc">
+                    This competition entry won a runner-up spot in the
+                    competition. It used data from various low-code trend
+                    industry reports as well as Kaggle survey data to look into
+                    the usage of these tools.
+                </div>
+            </div>
+            <img
+                src="/projects/Kaggle22/area-bi.png"
+                alt="area charts"
+                class="grid-item--area-bi grid-item"
+            />
+            <img
+                src="/projects/Kaggle22/line-chart.png"
+                alt="line chart"
+                class="grid-item--line-chart grid-item"
+            />
+            <img
+                src="/projects/Kaggle22/table.png"
+                alt="table"
+                class="grid-item--table grid-item"
+            />
+        </div>
+    </div>
+</div>
+
+<style>
+
+    /* GRID STYLING */
+
+    .results-grid {
+        background-color: #aec0b8;
+
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        column-gap: 10px;
+        grid-template-rows: repeat(3, 200px);
+        row-gap: 10px;
+    }
+
+    .grid-item--result {
+        grid-row: 1/2;
+        grid-column: 1/2;
+
+        box-shadow: none !important;
+        padding-right: 20px;
+    }
+
+    .grid-item--result .work-field-title {
+        margin-top: 0;
+    }
+
+    .grid-item--table {
+        grid-row: 3/4;
+        grid-column: 2/4;
+    }
+
+    .grid-item--area-bi {
+        grid-row: 1/3;
+        grid-column: 2/4;
+    }
+
+    .grid-item--line-chart {
+        grid-row: 2/4;
+        grid-column: 1/2;
+    }
+</style>
