00c16816609ef08f11b4e92391b0efe95e7f7b0c
schubert-da <schubertdeabreu2@gmail.com>
2024-01-07 10:56:54 +0530

    change work container background

--- src/components/works-section/WorksSection.svelte
+++ src/components/works-section/WorksSection.svelte
@@ -37,6 +37,7 @@
         display: flex;
         flex-direction: column;
 
+        background-color: #e0e0e0;
         border: 1px solid #999;
         border-radius: 4px;
         box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
